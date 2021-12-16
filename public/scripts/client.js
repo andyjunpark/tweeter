4b1c6f6485c3f0e74ac765f4dd3990aeed57802d
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-12-16 03:06:41 +0000

    added loadtweets AJAX get request for rendering tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,6 @@
  */
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-
   const createTweetElement = function(tweet) {
     let timestamp = timeago.format(tweet.created_at);
     let $tweet = $(
@@ -68,8 +42,16 @@ $(document).ready(function() {
       $('#tweets-container').append(tweet);
     };
   };
+ 
+  const loadtweets = function() {
+    $.ajax('/tweets', { method: 'GET'})
+    .then(function (moreTweets) {
+      console.log('Successfully get: ', moreTweets);
+      renderTweets(moreTweets);
+    });
+  };
 
-  renderTweets(data);
+  loadtweets();
 
   $(function() {
     const $form = $('#tweet-form');
