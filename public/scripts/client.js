68c3c669a3520b81fc319ad1c68c5983ad46d66b
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-12-16 01:52:27 +0000

    setup renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,28 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-// const $tweet = createTweetElement(tweetData);
 
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet);
-
-const createTweetElement = function(tweetData) {
+const createTweetElement = function(tweet) {
   let timestamp = timeago.format(tweetData.created_at);
-  let tweetMarkup = $(
+  let $tweet = $(
   `
   <article>
     <header>
@@ -48,5 +55,15 @@ const createTweetElement = function(tweetData) {
     </footer>
   </article>
   `);
-  $(".tweet").append(tweetMarkup);
+  return $tweet;
 };
+
+$('#tweets-container').append($tweet);
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+};
+
+renderTweets(data);
